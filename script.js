ff51243d6663d01414c43b35c0b9c1f11cddc2a3
NoahEspi <97457166+NoahEspi@users.noreply.github.com>
2022-08-04 10:36:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function encrypt(){
   var message = document.getElementById('messageToEnc').value;
   var secretPassphrase = document.getElementById('encPass').value;
 
-  if (message == "" || secretPassphrase == ""){
-    document.getElementById('EncryptedMessage').innerHTML = "Message or Passcode is blank!";
+  if (message == ""){
+    document.getElementById('EncryptedMessage').innerHTML = "Message is blank!";
   }
   else {
     var encrypted = CryptoJS.AES.encrypt(message, secretPassphrase);
@@ -17,8 +17,8 @@ function decrypt(){
   var message = document.getElementById('messageToDec').value;
   var secretPassphrase = document.getElementById('decPass').value;
 
-  if (message == "" || secretPassphrase == ""){
-    document.getElementById('DecryptedMessage').innerHTML = "Message or Passcode is blank!";
+  if (message == ""){
+    document.getElementById('DecryptedMessage').innerHTML = "Message is blank!";
   }
   else {
     var decrypted = CryptoJS.AES.decrypt(message, secretPassphrase);
